a14998c31118b2a5f73c0b3ebdf2ed0c93de1e8c
Wendler Eis <wendler@icasei.com.br>
2019-01-15 15:53:11 -0200

    feat(mobile): added list component

--- packages/shared/Starships/index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const URL_API = 'https://swapi.co/api'
-
-export const getStarships = () => {
-  return axios.get (`${URL_API}/starships`).then(res => {  
-    return res.data.results
-  })
-}
--- packages/web/src/components/List/List.js
+++ packages/web/src/components/List/List.js
@@ -1,18 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class List extends Component {
-  render() {
-    const { collection } = this.props
-      
-    return (
-      <div>
-        <h2>LISTA DE NAVES STARWARS</h2>
-        <ul>
-          {collection.map(item => (
-            <li key={item.name}>{item.name}</li>
-          ))}
-        </ul>
-      </div>
-    )
-  }
-}
+export default ({ collection }) => (
+  <div>
+    <h2>LISTA DE NAVES STARWARS</h2>
+    <ul>
+      {collection.map(item => (
+        <li key={item.id.videoId}>{item.snippet.title}</li>
+      ))}
+    </ul>
+  </div>
+)
