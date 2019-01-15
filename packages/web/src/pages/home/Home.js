38e805f4a3a1cec2fe6264600460fc1948b47f10
Wendler Eis <wendler@icasei.com.br>
2019-01-15 17:17:23 -0200

    feat(web): add separated style file

--- packages/web/src/pages/home/Home.js
+++ packages/web/src/pages/home/Home.js
@@ -3,9 +3,10 @@ import List from '../../components/List'
 import { getVideos } from 'shared/Videos'
 
 import { AppBar, Toolbar, IconButton, Typography, InputBase, withStyles } from '@material-ui/core';
-import { fade } from '@material-ui/core/styles/colorManipulator';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
+
+import homeStyle from './Home.style'
 
 class Home extends Component {
   state = {
@@ -27,10 +28,13 @@ class Home extends Component {
             <IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
               <MenuIcon />
             </IconButton>
+            
             <Typography className={classes.title} variant="h6" color="inherit" noWrap>
-              Material-UI
+              Youtube list videos
             </Typography>
+            
             <div className={classes.grow} />
+            
             <div className={classes.search}>
               <div className={classes.searchIcon}>
                 <SearchIcon />
@@ -51,64 +55,6 @@ class Home extends Component {
   }
 }
 
-const styles = theme => ({
-  root: {
-    width: '100%',
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-  title: {
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
-    },
-  },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing.unit,
-      width: 'auto',
-    },
-  },
-  searchIcon: {
-    width: theme.spacing.unit * 9,
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-    width: '100%',
-  },
-  inputInput: {
-    paddingTop: theme.spacing.unit,
-    paddingRight: theme.spacing.unit,
-    paddingBottom: theme.spacing.unit,
-    paddingLeft: theme.spacing.unit * 10,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: 120,
-      '&:focus': {
-        width: 200,
-      },
-    },
-  },
-});
+const styles = theme => ({...homeStyle(theme)})
 
 export default withStyles(styles)(Home)
